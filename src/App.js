0e45bd0716f5326f2ce72d36dd9fd9a7e84e5a60
CostelusP <pamparaucostelus1234@gmail.com>
2021-04-16 00:40:03 +0300

    Implement the authentication feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import styled from "styled-components";
+import { Authentication } from "./components/auth/index";
+import imageLogin from './assets/images/loginImage.jpg';
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background:url(${imageLogin}) no-repeat center center fixed;
+  background-size: cover;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Authentication />
+    </AppContainer>
   );
 }
 
