b0ed8ce90634add70d4666f59551213372279070
CostelusP <pamparaucostelus1234@gmail.com>
2021-04-17 01:43:09 +0300

    Setup the eslint and prettier for project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-
-import "./App.css";
-import styled from "styled-components";
-import { Authentication } from "./components/auth/index";
-import imageLogin from './assets/images/loginImage.jpg';
+import './App.css'
+import styled from 'styled-components'
+import { Authentication } from './components/auth/index'
+import imageLogin from './assets/images/loginImage.jpg'
 
 const AppContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background:url(${imageLogin}) no-repeat center center fixed;
-  background-size: cover;
-`;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	background: url(${imageLogin}) no-repeat center center fixed;
+	background-size: cover;
+`
 
 function App() {
-  return (
-    <AppContainer>
-      <Authentication />
-    </AppContainer>
-  );
+	return (
+		<AppContainer>
+			<Authentication />
+		</AppContainer>
+	)
 }
 
-export default App;
+export default App
--- src/common/constants.js
+++ src/common/constants.js
@@ -1,22 +1,20 @@
 export const backdropVariants = {
-    expanded: {
-      width: "233%",
-      height: "1050px",
-      borderRadius: "20%",
-      transform: "rotate(60deg)",
-    },
-    collapsed: {
-      width: "160%",
-      height: "550px",
-      borderRadius: "50%",
-      transform: "rotate(60deg)",
-    },
-  };
+	expanded: {
+		width: '233%',
+		height: '1050px',
+		borderRadius: '20%',
+		transform: 'rotate(60deg)',
+	},
+	collapsed: {
+		width: '160%',
+		height: '550px',
+		borderRadius: '50%',
+		transform: 'rotate(60deg)',
+	},
+}
 
-  export const expandingTransition = {
-    type: "spring",
-    duration: 2.3,
-    stiffness: 30,
-  };
-
-
+export const expandingTransition = {
+	type: 'spring',
+	duration: 2.3,
+	stiffness: 30,
+}
